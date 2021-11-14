471276af8a1849bbedd9d5139f063b48c47e24e8
joao16mariano <33557521+joao16mariano@users.noreply.github.com>
2021-11-14 18:29:34 -0300

    Add files via upload

---
+++ Joao14-11-2021/ProjetoFirebase/script.js
@@ -0,0 +1,12 @@
+
+function display(form){
+if (form.username.value=="admin") { 
+		if (form.password.value=="aqws") {              
+										location="https://projetocasa-30c30.web.app/paginas/index.html" 
+											} else {
+													alert("Senha ou nome incorretos")
+													}
+								} else {  alert("Senha ou nome incorretos")
+										}
+						}
+			
